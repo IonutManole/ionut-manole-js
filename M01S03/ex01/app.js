3717e353911b0b5ee8693ed1402b64d60313755b
Ionut Manole <ionutmanole@Ionuts-MacBook-Pro.local>
2020-10-08 22:05:33 +0300

    Started curs03

---
+++ M01S03/ex01/app.js
@@ -0,0 +1,15 @@
+console.log(object.name);
+console.log(object.age);
+console.log(
+  'Ma numesc ' +
+    object.name +
+    ' ' +
+    object.surname +
+    ' si am ' +
+    object.age +
+    ' ani.',
+);
+
+// Afiseaza anul in care aveai jumatate din varsta pe care ai trecut-o
+// in obiect scazand din anul curent (un proces numit hardcodare)
+console.log(2020 - object.age / 2);
