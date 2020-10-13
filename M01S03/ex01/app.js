26145709dd92bce4dadafb1ab64eba11b3f6c561
Ionut Manole <ionutmanole@Ionuts-MacBook-Pro.local>
2020-10-13 16:50:20 +0300

    Tema 8 Oct

--- M01S03/ex01/app.js
+++ M01S03/ex01/app.js
@@ -13,3 +13,11 @@ console.log(
 // Afiseaza anul in care aveai jumatate din varsta pe care ai trecut-o
 // in obiect scazand din anul curent (un proces numit hardcodare)
 console.log(2020 - object.age / 2);
+
+// Tema
+
+// Afiseaza fraza “xxx yyy are xx ani.” in consola folosind operatorul de concatenare
+console.log(object.name + ' ' + object.surname + ' are ' + object.age + ' ani');
+
+// Afiseaza anul in care te-ai nascut scazand varsta din anul curent
+console.log(2020 - object.age);
