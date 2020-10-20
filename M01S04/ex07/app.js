1d22a4b65a590250fdbf5647770261f7ab22b074
Ionut Manole <ionutmanole@Ionuts-MacBook-Pro.local>
2020-10-20 22:02:05 +0300

    Curs 04 for exercises

---
+++ M01S04/ex07/app.js
@@ -0,0 +1,26 @@
+let friends = [
+  {
+    name: 'Ionut',
+    surname: 'Manole',
+  },
+  {
+    name: 'Larry',
+    surname: 'Larryson',
+  },
+  {
+    name: 'Steven',
+    surname: 'Stevenson',
+  },
+  {
+    name: 'Carol',
+    surname: 'Carolson',
+  },
+  {
+    name: 'Andra',
+    surname: 'Andrason',
+  },
+  {
+    name: 'Ragnar',
+    surname: 'Ragnarson',
+  },
+];
