ab849a322530509b497a1f6e71d5ece2fff3f16d
Ionut Manole <ionutmanole@Ionuts-MacBook-Pro.local>
2020-11-03 21:59:32 +0200

    Curs06 part1

---
+++ M01S06/ex02/app.js
@@ -0,0 +1,15 @@
+// better use feature detection
+
+let paragraphElement = document.createElement('p');
+let userAgentString = navigator.userAgent;
+let message = 'Nu stim ce browser folosesti.';
+
+if (
+  userAgentString.indexOf('Chrome') >= 0 ||
+  userAgentString.indexOf('Firefox') >= 0
+) {
+  message = 'Navighezi folosind Chrome';
+}
+
+paragraphElement.innerText = message;
+document.body.appendChild(paragraphElement);
